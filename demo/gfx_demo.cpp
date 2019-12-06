1961b9709fd7842b7fd30b84825911ea3dfe8d54
daemyung jang <dm86.jang@gmail.com>
2019-12-06 09:56:40 +0900

    Support CLI in the demo

--- demo/gfx_demo.cpp
+++ demo/gfx_demo.cpp
@@ -3,8 +3,14 @@
 // See "LICENSE" for license information.
 //
 
+#include <cstdint>
+#include <string>
+#include <cxxopts.hpp>
 #include "Gfx_triangle_demo.h"
 #include "Gfx_texture_demo.h"
+
+using namespace std;
+using namespace cxxopts;
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -18,8 +24,22 @@ void run(uint32_t w, uint32_t h)
 
 int main(int argc, char* argv[])
 {
-    // run<Gfx_triangle_demo>(360, 640);
-    run<Gfx_texture_demo>(360, 640);
+    Options options("gfx_demo", "Various gfx demos");
+
+    options.add_options()
+        ("w, width", "window width", value<uint32_t>()->default_value("360"))
+        ("h, height", "window height", value<uint32_t>()->default_value("640"))
+        ("t, target", "demo name", value<string>()->default_value("triangle"));
+
+    auto result = options.parse(argc, argv);
+    auto target = result["target"].as<string>();
+
+    if ("triangle" == target)
+        run<Gfx_triangle_demo>(result["w"].as<uint32_t>(), result["h"].as<uint32_t>());
+    else if("texture" == target)
+        run<Gfx_texture_demo>(result["w"].as<uint32_t>(), result["h"].as<uint32_t>());
+
+    return 0;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
