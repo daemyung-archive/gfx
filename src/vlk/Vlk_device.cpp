b2b6d715376e5ef6204368b92f912a532ae85041
daemyung jang <dm86.jang@gmail.com>
2019-12-27 23:12:16 +0900

    Delete an unused function

--- src/vlk/Vlk_device.cpp
+++ src/vlk/Vlk_device.cpp
@@ -44,24 +44,6 @@ inline constexpr auto all_flags(uint32_t src, uint32_t test) noexcept
 
 //----------------------------------------------------------------------------------------------------------------------
 
-VkBool32 on_debug_report(VkDebugReportFlagsEXT flags,
-                         VkDebugReportObjectTypeEXT objectType, uint64_t object,
-                         size_t location,
-                         int32_t messageCode, const char* pLayerPrefix, const char* pMessage,
-                         void* pUserData)
-{
-    if (VK_DEBUG_REPORT_ERROR_BIT_EXT & flags) {
-        // todo: print debug report message.
-    }
-    if (VK_DEBUG_REPORT_DEBUG_BIT_EXT & flags) {
-        // todo: print debug report message.
-    }
-
-    return VK_FALSE;
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-
 } // of namespace
 
 namespace Gfx_lib {
